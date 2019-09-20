03e1474e328ae4d28fb250e10ea781c5aaa75199
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-20 03:58:54 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -16,5 +16,6 @@ def total_tardies_for_all_students
   "SELECT SUM(tardies) FROM students"
 end
 
-def average_gpa_for_9th_grade
+def average_gpa_for_9th_grade 
+  "SELECT AVG(gpa) FROM students WHERE grade = 9"
 end
